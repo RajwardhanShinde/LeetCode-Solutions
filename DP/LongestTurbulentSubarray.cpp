869f4cd1c5e3fc4636d69bd802b85118d0084b0e
RajShinde07 <rajshinde55553@gmail.com>
2020-06-12 20:49:23 +0530

    Added LongestTurbulentSubarray

---
+++ DP/LongestTurbulentSubarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& A) {
+        vector<vector<int>> dp(A.size(), vector<int>(2, 1));
+        int res = 1;
+        for(int i = 1; i < A.size(); i++) {
+            if(A[i] > A[i - 1]) 
+                dp[i][0] = dp[i - 1][1] + 1;
+            else if(A[i] < A[i - 1])
+                dp[i][1] = dp[i - 1][0] + 1;
+            res = max({res, dp[i][0], dp[i][1]});
+        } 
+        return res;
+    }
+};
